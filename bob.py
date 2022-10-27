4c82efafc6299b7796a779daa2f7cf1d0e7aa9cc
shreeja-shreeja <115733593+shreeja-shreeja@users.noreply.github.com>
2022-10-27 22:15:25 +0200

    Update bob.py

--- bob.py
+++ bob.py
@@ -6,7 +6,7 @@ def response(hey_bob):
             return "Whoa, chill out!"
     elif("Bob" in hey_bob):
         return "Fine. Be that way!"
-    elif(len(hey_bob)==0):
+    elif(!(hey_bob.isalpha())):
         return "Whatever."
     elif(hey_bob[-1]=="?"):
         return "Sure."
