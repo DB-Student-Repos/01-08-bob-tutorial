f4fee4149059b81c377476a1cb3b7c49cb476827
shreeja-shreeja <115733593+shreeja-shreeja@users.noreply.github.com>
2022-10-27 19:27:20 +0200

    Update bob.py

--- bob.py
+++ bob.py
@@ -1,2 +1,13 @@
 def response(hey_bob):
-    pass
+    if(hey_bob.isupper()):
+        if(hey_bob[-1]=="?"):
+            return "Calm down, I know what I'm doing!"
+        else:
+            return "Whoa, chill out!"
+    elif("Bob" in hey_bob):
+        return "Fine. Be that way!"
+    elif(hey_bob[-1]=="?"):
+        return "Sure."
+    else:
+        return "whatever"
+    
