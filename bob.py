580ff5ca66eb8b5946272e539ea6710a98f89ab6
Shreeja Shreeja <115733593+shreeja-shreeja@users.noreply.github.com>
2022-11-28 22:48:00 +0100

    Update bob.py

--- bob.py
+++ bob.py
@@ -2,13 +2,17 @@
 
 
 def response(hey_bob):
-    if (hey_bob.strip()[-1] == "?"):
-        return "Sure."
-    elif (hey_bob.isupper()):
-        return "Calm down, I know what I'm doing."
-    elif (len(hey_bob.strip()) == 0):
-        return "Fine. Be that way."
+    var = hey_bob.strip()
+    if (len(var) == 0):
+        return "Fine. Be that way!"
+    elif (var[-1] == "?"):
+        if (var.isupper()):
+            return "Calm down, I know what I'm doing!"
+        else:
+            return "Sure."
+    elif (var.isupper()):
+        return "Whoa, chill out!"
     else:
         return "Whatever."
-          
+    
     
